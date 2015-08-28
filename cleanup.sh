8b24f722fa9f1b416da4e4e675a09a3998b06156
JahMyst <olivier.cervello@gmail.com>
2015-08-27 20:44:05 -0500

    first commit

---
+++ cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source ../init.sh
+### CLEANUP.SH ###
+
+##############################
+### HOW TO USE THIS SCRIPT ###
+##############################
+
+# ./cleanup.sh
+
+#############################
+### WHAT THIS SCRIPT DOES ###
+#############################
+
+# Cleanup old log files present in 'archive' folder.
+
+##############
+### SCRIPT ###
+##############
+
+echo "===> Cleaning up <==="
+sudo rm -rf $EasyAP/logs/*
+sudo rm -rf $EasyAP/archive/*
+echo " done."
